3965b4ea6ff827d6716628292d78748a37c0f0b0
balusena <balusena12@gmail.com>
2024-01-09 22:08:49 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -123,19 +123,20 @@ resource "aws_security_group" "sg" {
   description = "${var.component}-${var.env}-sg"
   vpc_id = var.vpc_id
 
-  ingress{
+  ingress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress{
+  egress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "${var.component}-${var.env}-sg"
   }
