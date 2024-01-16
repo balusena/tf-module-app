59ae87ad5c887025ffe30c0a8b0e81558abb133c
balusena <balusena12@gmail.com>
2024-01-16 15:16:30 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -1,133 +1,13 @@
-##Creating AWS IAM Policy
-#resource "aws_iam_policy" "policy" {
-#  name        = "${var.component}-${var.env}-ssm-pm-policy"
-#  path        = "/"
-#  description = "${var.component}-${var.env}-ssm-pm-policy"
-#
-#  policy = jsonencode({
-#    "Version": "2012-10-17",
-#    "Statement": [
-#      {
-#        "Sid": "VisualEditor0",
-#        "Effect": "Allow",
-#        "Action": [
-#          "ssm:GetParameterHistory",
-#          "ssm:GetParametersByPath",
-#          "ssm:GetParameters",
-#          "ssm:GetParameter"
-#        ],
-#        "Resource": "arn:aws:ssm:us-east-1:828448425071:parameter/roboshop.${var.env}.${var.component}.*"
-#      }
-#    ]
-#  })
-#}
-#
-##Creating AWS IAM Role
-#resource "aws_iam_role" "role" {
-#  name = "${var.component}-${var.env}-ec2-role"
-#  assume_role_policy = jsonencode({
-#    Version = "2012-10-17"
-#    Statement = [
-#      {
-#        Action = "sts:AssumeRole"
-#        Effect = "Allow"
-#        Sid    = ""
-#        Principal = {
-#          Service = "ec2.amazonaws.com"
-#        }
-#      },
-#    ]
-#  })
-#}
-#
-##Creating AWS Instance Profile
-#resource "aws_iam_instance_profile" "instance_profile" {
-#  name = "${var.component}-${var.env}-ec2-role"
-#  role = aws_iam_role.role.name
-#}
-#
-##Attaching IAM Policy To IAM Role
-#resource "aws_iam_role_policy_attachment" "policy-attach" {
-#  role       = aws_iam_role.role.name
-#  policy_arn = aws_iam_policy.policy.arn
-#}
-#
-##Creating AWS Security Group
-#resource "aws_security_group" "sg" {
-#  name        = "${var.component}-${var.env}-sg"
-#  description = "${var.component}-${var.env}-sg"
-#
-#  ingress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#
-#  tags = {
-#    Name = "${var.component}-${var.env}-sg"
-#  }
-#}
-#
-## Creating AWS EC2 Instance
-#resource "aws_instance" "instance" {
-#  ami           = data.aws_ami.ami.id
-#  instance_type = "t3.small"
-#  vpc_security_group_ids = [aws_security_group.sg.id]
-#  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
-#
-#  tags = merge({
-#    Name = "${var.component}-${var.env}"
-#    },
-#    var.tags)
-#}
-#
-## Creating an AWS Route 53 DNS record in the specified hosted zone
-#resource "aws_route53_record" "dns" {
-#  zone_id = "Z09157091J32F5PJ5K67Y"
-#  name    = "${var.component}-dev"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.instance.private_ip]
-#}
-#
-## Creating NULL resources to run Ansible playbooks in preferred env from a GitHub repository establishing SSH connection
-#resource "null_resource" "ansible" {
-#  depends_on = [aws_instance.instance, aws_route53_record.dns]
-#
-#  provisioner "remote-exec" {
-#    connection {
-#      type     = "ssh"
-#      user     = "centos"
-#      password = "DevOps321"
-#      host     = aws_instance.instance.public_ip
-#    }
-#
-#    inline = [
-#      "sudo labauto ansible",
-#      "sudo set-hostname -skip-apply ${var.component}",
-#      "ansible-pull -i localhost, -U https://github.com/balusena/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}"
-#    ]
-#  }
-#}
-
-resource "aws_security_group" "sg" {
+resource "aws_security_group" "main" {
   name        = "${var.component}-${var.env}-sg"
   description = "${var.component}-${var.env}-sg"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_subnets_cidr
   }
 
   egress {
@@ -142,17 +22,59 @@ resource "aws_security_group" "sg" {
   }
 }
 
-resource "aws_instance" "test" {
+resource "aws_launch_template" "main" {
+  name = "${var.component}-${var.env}"
 
-  ami = data.aws_ami.ami.id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = var.subnet_id
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
 
-  tags = {
-    Name = var.component
+  image_id = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.main.id ]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = merge({ Name = "${var.component}-${var.env}", Monitor = "true" }, var.tags)
+  }
+
+  user_data              = base64encode(templatefile("${path.module}/userdata.sh", {
+    env                  = var.env
+    component            = var.component
+  }))
+
+#  block_device_mappings {
+#    device_name = "/dev/sda1"
+#
+#    ebs {
+#      volume_size = 10
+#      encrypted   = "true"
+#      kms_key_id  = var.kms_key_id
+#    }
+# }
+}
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
   }
 }
+
+## Creating an AWS Route 53 DNS record in the specified hosted zone
+#resource "aws_route53_record" "dns" {
+#  zone_id = "Z09157091J32F5PJ5K67Y"
+#  name    = "${var.component}-dev"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.instance.private_ip]
+#}
+
 
 
 
--- variables.tf
+++ variables.tf
@@ -3,5 +3,12 @@ variable "component" {}
 variable "tags" {
    default = {}
 }
-variable "subnet_id" {}
+variable "subnets" {}
 variable "vpc_id" {}
+variable "app_port" {}
+variable "sg_subnets_cidr" {}
+variable "instance_type" {}
+variable "kms_key_id" {}
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "max_size" {}
