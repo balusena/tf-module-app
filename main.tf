fed2e7d7b2afb6f36da0484acae54d790fb0aee7
balusena <balusena12@gmail.com>
2024-01-16 21:43:50 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
-  target_group_arn   = [ aws_lb_target_group.main.arn ]
+  target_group_arns  = [ aws_lb_target_group.main.arn ]
 
   launch_template {
     id      = aws_launch_template.main.id
