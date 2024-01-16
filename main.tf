32232556778a34f5b236a1363da88816724096c4
balusena <balusena12@gmail.com>
2024-01-16 21:49:32 +0000

    roboshop terraform files

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_lb_target_group" "main" {
   vpc_id   = var.vpc_id
 }
 
-resource "aws_lb_listener_rule" "static" {
+resource "aws_lb_listener_rule" "main" {
   listener_arn = var.listener_arn
   priority     = var.lb_rule_priority
 
